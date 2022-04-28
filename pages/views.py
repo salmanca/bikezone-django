ed11f56c080dbc4dfc5a4ef26a8472cac145b47c
salmanca <mohammedsalmanfaris.c.a@gmail.com>
2022-04-28 22:18:37 +0530

    static files and base templates

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # home page
 
 def home(request):
-    return render(request,"pages/home.html")
+    return render(request,"pages/index.html")
+
+def about(request):
+    return render(request, "pages/about.html")
+
+def services(request):
+    return render(request, "pages/services.html")
+
+def contact(request):
+    return render(request, "pages/contact.html")
